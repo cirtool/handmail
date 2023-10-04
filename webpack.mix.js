44018ce9de1db97e36e34499aa47b2ce338dee03
Luis Medina <git@lmedinam.es>
2023-10-04 09:41:52 -0400

    Fix wrong output CSS path

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ let mix = require("laravel-mix");
 
 mix
     .js("resources/js/app.js", "public")
-    .postCss("resources/css/app.css", "public", [
+    .postCss("resources/css/app.css", "/", [
         require("tailwindcss"),
     ])
     .setPublicPath("public")
