426c24c493dcb94f8d5a896517fe87b4051a9f45
Luis Medina <git@lmedinam.es>
2024-01-14 08:31:33 -0400

    Add redirect from `/` to `/templates`

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Cirtool\Handmail\Controllers\Templates\TemplateWebview;
 use Cirtool\Handmail\Controllers\Templates\TemplateWebviewController;
 use Illuminate\Support\Facades\Route;
 use Cirtool\Handmail\Livewire\Templates\ShowAllTemplates;
@@ -12,6 +11,7 @@ if (config('handmail.panel.is_active', false)) {
         ->name('handmail.')
         ->middleware(config('handmail.panel.middlewares', 'web'))
         ->group(function () {
+            Route::get('/', fn () => redirect()->route('handmail.templates'));
             Route::get('/templates', ShowAllTemplates::class)->name('templates');
             Route::get('/templates/{template}/edit', EditTemplate::class)->name('edit-template');
             Route::get('/templates/{template}/webview', TemplateWebviewController::class)->name('template-webview');
